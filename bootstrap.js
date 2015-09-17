520eb7fddd53e8564c45ba833df4a28b2916744b
Christoffer Rødvik <christoffer@kodemon.net>
2015-09-17 17:04:46 +0900

    now uses user-service to create config defined users

--- bootstrap.js
+++ bootstrap.js
@@ -1,37 +1,35 @@
 'use strict';
 
-let codes    = require('./lib/errors');
-let password = require('./lib/password-service');
-let User     = Reach.model('User');
-let error    = Reach.Error;
-let type     = Reach.Helpers.Type;
-let config   = Reach.config.user;
+let codes       = require('./lib/errors');
+let userService = require('./lib/user-service');
+let User        = Reach.model('User');
+let error       = Reach.Error;
+let type        = Reach.Helpers.Type;
+let config      = Reach.config.user;
 
 module.exports = function *() {
   let count = yield User.count();
   if (!count) {
-    let admins = getAdmins();
-    for (let i = 0, len = admins.length; i < len; i++) {
-      let user = new User(admins[i]);
-      yield password.create(user.password);
-      yield user.save();
+    let users = getUsers();
+    for (let i = 0, len = users.length; i < len; i++) {
+      yield userService.create(users[i]);
     }
   }
 };
 
 /**
- * @method getAdmins
+ * @method getUsers
  * @return {Array}
  */
-function getAdmins() {
+function getUsers() {
   if (!config) {
     throw error.parse(codes.CONFIG_MISSING);
   }
-  if (!config.admins) {
+  if (!config.users) {
     throw error.parse(codes.CONFIG_MISSING_ADMINS);
   }
-  if (!type.isArray(config.admins)) {
+  if (!type.isArray(config.users)) {
     throw error.parse(codes.CONFIG_INVALID_ADMINS_TYPE);
   }
-  return config.admins;
+  return config.users;
 }
